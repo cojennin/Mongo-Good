3f88cc978a018f8a9291854484157d2c1b20e165
Connor Jennings <cojennin@gmail.com>
2014-04-17 00:26:33 -0400

    Start implementation of POST and GET. Separating out the functionality of each.
    As of this commit, the libbson library doesn't support parsing JSON into
    BSON. Just BSON into JSON. We need to either 1) manually parse the JSON
    into BSON or 2) Utilize some kind of library.
    
    And by 1), it looks like it might be useful to use the MDBWrapper's
    json_to_bson parsing. This is just a simple function to take a json
    object, cycle through it by key value and append those key/value pairs
    to a corresponding BSON object. Not too crazy.

--- ngx_http_mg_module.c
+++ ngx_http_mg_module.c
@@ -17,6 +17,11 @@ typedef struct {
   ngx_str_t     mongo_passw;
 } ngx_http_mg_conf_t;
 
+typedef struct {
+  ngx_str_t     error_msg;
+  ngx_int_t     error_code;
+} error_s
+
 static ngx_command_t ngx_http_mg_commands[] = {
   {
     ngx_string("mongo"),
@@ -95,31 +100,65 @@ ngx_http_mg_handler(ngx_http_request_t *r)
     ngx_http_mg_conf_t  *mgcf;
     ngx_http_complex_value_t cv;
     char* response;
+    error_s* error;
 
+    //Let's assume we can handle the following.
+    //GET will get
+    //POST will create/update
+    //Todo: What about PUT?
+    if (!(r->method & (NGX_HTTP_GET|NGX_HTTP_POST))) {
+        return NGX_HTTP_NOT_ALLOWED;
+    }
+
+    if (r->headers_in.if_modified_since) {
+        return NGX_HTTP_NOT_MODIFIED;
+    }
+
+    mgcf = ngx_http_get_module_loc_conf(r, ngx_http_mg_module);
+
+    //If we've got a GET request, process it
+    if(r->method == NGX_HTTP_GET) {
+      if(ngx_http_mg_handle_get_request(r, mgcf, &cv, error) == NULL) {
+        ngx_memzero(&cv, sizeof(ngx_http_complex_value_t));
+        cv.value.len = error->error_msg.len;
+        cv.value.data = error->error_msg;
+
+        return ngx_http_send_response(r, error->error_code, &application_type, &cv);
+      }
+    }
+
+    //Duplicative, can refactor.
+    if(r->method == NGX_HTTP_POST) {
+      if(ngx_http_mg_handle_post_request(r, mgcf, &cv, error) == NULL) {
+        ngx_memzero(&cv, sizeof(ngx_http_complex_value_t));
+        cv.value.len = error->error_msg.len;
+        cv.value.data = error->error_msg;
+
+        return ngx_http_send_response(r, error->error_code, &application_type, &cv);
+      }
+    }
+
+    return ngx_http_send_response(r, NGX_HTTP_OK, &application_type, &cv);
+}
+
+//Build out the complex value from our query and modify our
+//request appropriately (handle the application type, etc).
+static char*
+ngx_http_mg_handle_get_request(ngx_http_request_t *r, ngx_http_mg_conf_t mgcf*, ngx_http_complex_value_t* cv, error_s* ngx_mg_req_error) {
     //Mongoc related vars
-    mongoc_client_t *client;
+    mongo_client_t *client;
     mongoc_collection_t *collection;
     mongoc_cursor_t *cursor;
 
-    //Bson related vars
+    //Dealing with bson here.
     bson_error_t error;
     const bson_t *doc;
     bson_t doc_response = BSON_INITIALIZER;
     bson_t query;
 
-    if (!(r->method & (NGX_HTTP_GET|NGX_HTTP_POST))) {
-        return NGX_HTTP_NOT_ALLOWED;
-    }
-
-    mgcf = ngx_http_get_module_loc_conf(r, ngx_http_mg_module);
-
-    mongoc_init(); //Initialize mongoc
-    client = mongoc_client_new(mgcf->mongo_server_addr.data); //Connect to db
-
-    //Guess a 500 should suffice?
-    if(!client) {
-      return NGX_HTTP_INTERNAL_SERVER_ERROR;
-    }
+    //Init our client and connect
+    mongoc_init();
+    client = mongoc_client_new(mgcf->mongo_server_addr.data); //Connect to db. Let's assume for the moment all actions require connection.
 
     bson_init( &query ); //init bson
 
@@ -128,7 +167,10 @@ ngx_http_mg_handler(ngx_http_request_t *r)
 
     //Also a 500
     if(!collection) {
-      return NGX_HTTP_INTERNAL_SERVER_ERROR;
+      ngx_mg_req_error = malloc(sizeof(error_s));
+      ngx_mg_req_error->error_msg = (ngx_str_t)ngx_string("{ \"ok\" : false, \"reason\" : \"Unable to get collection.\" }");
+      ngx_mg_req_error->error_code = NGX_HTTP_INTERNAL_SERVER_ERROR;
+      return NULL;
     }
 
     cursor = mongoc_collection_find(collection, MONGOC_QUERY_NONE, 0, 0, 0, &query, NULL, NULL);
@@ -144,31 +186,163 @@ ngx_http_mg_handler(ngx_http_request_t *r)
     //Ok, need a large enough string to contain our docs + extra info.
     response = (char*)ngx_palloc(r->pool, strlen(str) + 20 );
 
-    strncpy(response, "{\"q_results\" : [", 16);
-    strncpy(response + 16, str, strlen(str));
-    strncpy(response + 16 + strlen(str), "] }\0", 4); //Wrap up and terminate.
+    //If we couldn't allocate, fail.
+    if( !response ) {
+      ngx_mg_req_error = malloc(sizeof(error_s));
+      ngx_mg_req_error->error_msg = (ngx_str_t)ngx_string("{ \"ok\" : false, \"reason\" : \"Unable to allocate enough memory for a response.\" }");
+      ngx_mg_req_error->error_code = NGX_HTTP_INTERNAL_SERVER_ERROR;
+      return NULL;
+    }
+
+    //Get rid of strncpy, we're well aware of the size being allocated.
+    strpy(response, "{\"q_results\" : [");
+    strcpy(response, str);
+    strcpy(response, "] }\0"); //Wrap up and terminate.
 
     bson_free(str); //Free up str.
 
     if(mongoc_cursor_error(cursor, &error)) {
-      return NGX_HTTP_INTERNAL_SERVER_ERROR;
+      ngx_mg_req_error = malloc(sizeof(error_s));
+      ngx_mg_req_error->error_msg = (ngx_str_t)ngx_string("{ \"ok\" : false, \"reason\" : \"There was a problem with the Mongo Cursor.\" }");
+      ngx_mg_req_error->error_code = NGX_HTTP_INTERNAL_SERVER_ERROR;
+      return NULL;
     }
 
     bson_destroy(&query);
     mongoc_cursor_destroy(cursor);
     mongoc_collection_destroy(collection);
-    mongoc_client_destroy(client);
 
-    ngx_memzero(&cv, sizeof(ngx_http_complex_value_t));
+    ngx_memzero(cv, sizeof(ngx_http_complex_value_t));
 
-    cv.value.len = strlen(response);
-    cv.value.data = response;
+    cv->value.len = strlen(response);
+    cv->value.data = response;
 
-    if (r->headers_in.if_modified_since) {
-        return NGX_HTTP_NOT_MODIFIED;
+    return 1;
+}
+
+//Handle posting to Mongo.
+static char *
+ngx_http_mg_handle_post_request(ngx_http_request_t* r, ngx_http_mg_conf_t* mgcf, ngx_http_complex_value_t* cv, char* errormsg) {
+    char uri*
+
+    //Mongoc related vars
+    mongo_client_t *client;
+    mongoc_collection_t *collection;
+    mongoc_cursor_t *cursor;
+
+    //Dealing with bson here.
+    bson_error_t error;
+    const bson_t *doc;
+    bson_t doc_response = BSON_INITIALIZER;
+    bson_t query;
+
+    //Init our client and connect
+    mongoc_init();
+
+    uri = build_conn_uri(mgcf->mongo_server_addr.data, mgcf->mongo_user.data, mgcf->mongo_pass.data);
+    client = mongoc_client_new(uri); //Connect to db. Let's assume for the moment all actions require connection.
+
+    if(!client) {
+      ngx_mg_req_error = malloc(sizeof(error_s));
+      ngx_mg_req_error->error_msg = (ngx_str_t)ngx_string("{ \"ok\" : false, \"reason\" : \"Could not authenticate." }");
+      ngx_mg_req_error->error_code = NGX_HTTP_UNAUTHORIZED;
+      return NULL;
     }
 
-    return ngx_http_send_response(r, NGX_HTTP_OK, &application_type, &cv);
+    //https://github.com/mongodb/mongo-c-driver/blob/master/doc/mongoc_collection_find.txt
+    collection = mongoc_client_get_collection(client, mgcf->mongo_database.data, mgcf->mongo_collection.data); //Get our requested collection
+
+    //Also a 500
+    if(!collection) {
+      ngx_mg_req_error = malloc(sizeof(error_s));
+      ngx_mg_req_error->error_msg = (ngx_str_t)ngx_string("{ \"ok\" : false, \"reason\" : \"Unable to get collection.\" }");
+      ngx_mg_req_error->error_code = NGX_HTTP_INTERNAL_SERVER_ERROR;
+      return NULL;
+    }
+
+    return 1;
+}
+
+static char *
+build_conn_uri(char* addr, char* username, char* pass)
+{
+     return bson_strdup_printf("mongodb://%s:%s@%s:27017",
+                                      pass,
+                                      username,
+                                      addr);
+}
+
+/**
+* Appends the correct bson type to the passed in bson object: used during json parsing, called for each
+* individual key \ value pair.
+* @param b: The existing bson to append the new object to.
+* @param val: The value of the key\value pair parsed (void).
+* @param key: The character key for the key\value pair passed.
+* @param r: The apache request (used for apr memory allocation).
+* @return void: Result is appended to passed in bson.
+*/
+static void json_key_to_bson_key (bson *b, void *val,const char *key,request_rec *r) {
+  // Because the mongo C driver currently doesn't drop out invalid keys in an insert,
+  // We have to destroy the k: v pair during a POST completely.
+  if (strspn("$",key) && r->method_number == M_POST)
+    return;
+
+  // Determine the type of the passed in value and call the correct bson_append function.
+  switch (json_object_get_type (val)) {
+    case json_type_boolean:
+      bson_append_bool (b, key, json_object_get_boolean (val));
+      break;
+    case json_type_double:
+      bson_append_double (b, key, json_object_get_double (val));
+      break;
+    case json_type_int: {
+      if (json_object_get_int64(val) > INT32_MAX)
+        bson_append_long (b, key, json_object_get_int64 (val));
+      else
+        bson_append_int (b, key, json_object_get_int64 (val));
+      break;
+    }
+    case json_type_object: {
+      // The json value type has recursive behavior - calling back to the json_to_bson function to parse
+      // the values internal to the nested json.
+      if (strcmp("_id", key) == 0) {
+        append_oid(b, val, key);
+        break;
+      }
+      bson *sub;
+      sub = json_to_bson (val, r);
+      bson_append_bson(b,key,sub);
+      bson_destroy (sub);
+      break;
+    }
+    case json_type_array: {
+      // To handle nested arrays.
+      int pos;
+      bson_append_start_array (b, key);
+      for (pos = 0; pos < json_object_array_length (val); pos++) {
+        char kk[10];
+        sprintf(kk,"%d",pos);
+        json_key_to_bson_key ( b, json_object_array_get_idx ( val, pos ), kk, r );
+      }
+      bson_append_finish_array (b);
+      break;
+    }
+    case json_type_string: {
+      const char * const strFromJSON = json_object_get_string(val);
+      const char * strVal = strFromJSON;
+      if (*strVal == '/' && strVal++) while(*strVal != '\0' && *strVal != '/') strVal++;
+      if (*strVal == '/') {
+        char * regex = (char*) apr_palloc(r->pool, strVal - strFromJSON - 1);
+        for (int i = 1; i < strVal - strFromJSON; i++) *(regex+i-1) = *(strFromJSON+i);
+        *(regex + (strVal - strFromJSON) - 1) = '\0';
+        bson_append_regex(b, key, regex, ++strVal);
+      }
+      else bson_append_string (b, key, strFromJSON);
+      break;
+    }
+    default:
+      break;
+  }
 }
 
 static char *
